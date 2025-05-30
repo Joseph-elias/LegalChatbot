980098cfe5ca696861b46b705630cbf7dea73673
Joseph-elias <josepheliaskh@gmail.com>
2025-05-30 19:04:35 +0200

    Save current work

--- frontend/src/components/QuestionInput.tsx
+++ frontend/src/components/QuestionInput.tsx
@@ -16,6 +16,7 @@ const QuestionInput: React.FC = () => {
 
     try {
       const res = await fetch('${import.meta.env.VITE_API_BASE}/search', {
+      
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ query: question, top_k: 3, alpha: 0.6 }),
